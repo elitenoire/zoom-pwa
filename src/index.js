4175919d1c1391d55bb05267e04417d3bc656103
Eva Raymond <evathecoderr@outlook.com>
2018-03-23 23:35:57 +0100

    bare minimum setup: uikit + service-worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import store from './store';
+// import './styles/main.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
